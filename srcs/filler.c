0e5e7172e0a84fa04d0c23c31a57827466b421a6
Marion SAKWINSKI <msakwins@e2r12p10.42.fr>
2017-07-13 18:13:14 +0200

    basile pls

--- srcs/filler.c
+++ srcs/filler.c
@@ -6,7 +6,7 @@
 /*   By: msakwins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 22:36:55 by msakwins          #+#    #+#             */
-/*   Updated: 2016/11/05 22:42:37 by msakwins         ###   ########.fr       */
+/*   Updated: 2017/07/13 18:12:20 by msakwins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int			main()
 {
+	int		fd;
 	int		ret;
+	char	buf[9999 + 1];
+	char	*tab;
 
-	ret = 1;
+	fd = open("filler.log", O_WRONLY | O_CREAT);
+	ret = read(0, buf, 9999);
+	buf[ret] = '\0';
+	write(fd, "----------\n", 12);
+	write(fd, buf, ret);
+	close(fd);
+	tab = ft+strdup(buf);
 	return (0);
 }
